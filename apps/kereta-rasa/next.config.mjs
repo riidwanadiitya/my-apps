cb7cebf61599ccf59a9166b491629e4b861b9215
ridwansaputra <riidwanadiitya@gmail.com>
2025-05-04 16:13:47 +0700

    adjust next config kereta rasa

--- apps/kereta-rasa/next.config.mjs
+++ apps/kereta-rasa/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: "export",
   transpilePackages: ["@workspace/ui"],
   images: {
     unoptimized: true,
