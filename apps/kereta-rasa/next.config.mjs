41cbeb2d792d2f713080d418a7d841f11db0aadb
ridwansaputra <riidwanadiitya@gmail.com>
2025-05-04 10:14:35 +0700

    adjust kereta-rasa next.config

--- apps/kereta-rasa/next.config.mjs
+++ apps/kereta-rasa/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   transpilePackages: ["@workspace/ui"],
   images: {
-    domains: ["fakestoreapi.com","picsum.photos"],
+    domains: ["fakestoreapi.com", "picsum.photos"],
   },
 };
 
